ef77e43284db0126a6471f8535199d1b22c92ec0
Ubuntu <ubuntu@ip-172-31-3-24.ap-northeast-2.compute.internal>
2024-02-29 00:31:42 +0900

    기능 구현 완성

--- core/strategy_0.py
+++ core/strategy_0.py
@@ -6,7 +6,31 @@ if __name__ == '__main__':
 		print(path.dirname( path.dirname( path.abspath(__file__) ) ))
 		sys.path.append(path.dirname( path.dirname( path.abspath(__file__) ) ))
 
+
 from interface.upbit import buycoin_mp, sellcoin_mp
+from entity.balance import krw_balance
+from entity.ticker_list import buy_list
 
+# wallet 점검
+for my_ticker in krw_balance.keys():
+	try:
+		if my_ticker not in buy_list.keys():
+			sellcoin_mp(my_ticker, str(krw[my_ticker]['balance']))
+	except Exception as e:
+		print(e)
 
-sellcoin_mp("KRW-XRP", str(7.34034744))
+# 차액 체크
+for buy_ticker in buy_list.keys():
+	print("buy_ticker:", buy_ticker)
+	try:
+		difference_balance = krw_balance['total'] * buy_list[buy_ticker] - krw_balance.get(buy_ticker, {}).get('balance_krw', 0)
+		print("difference_balance:", difference_balance)
+		if difference_balance > 0:
+			buycoin_mp(buy_ticker, str(difference_balance))
+			print("successed buy ", buy_ticker)
+		if difference_balance < 0:
+			difference_balance = difference_balance/krw_balance[buy_ticker].get('trade_price', 1)
+			sellcoin_mp(buy_ticker, str(-difference_balance))
+			print("successed sell ", buy_ticker)
+	except Exception as e:
+		print(e)
